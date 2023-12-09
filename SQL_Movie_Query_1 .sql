601762468aa426f21c02938e4b6561361922469a
CharlesDeWilde <132870993+CharlesDeWilde@users.noreply.github.com>
2023-12-09 14:47:50 -0800

    Add files via upload

---
+++ SQL_Movie_Query_1 .sql
@@ -0,0 +1,34 @@
+SELECT
+    film.film_id,
+    film.title AS Movie_Title,
+    film.description AS Movie_Description,
+    film.release_year AS Year_Released,
+    film.rental_duration AS Days_Rented,
+    film.rental_rate AS Rental_Cost,
+    film.length AS Runtime,
+    film.replacement_cost AS Replacement_Fee,
+    film.rating AS Rating, 
+    film.last_update AS Last_Updated,
+    film.special_features AS Special_Features,
+    film.fulltext
+FROM
+    film
+JOIN
+    film_category ON film.film_id = film_category.film_id
+WHERE
+    release_year >= 2005
+GROUP BY
+    film.film_id,  -- Making sure to include all non-aggregated columns in the GROUP BY
+    film.title,
+    film.description,
+    film.release_year,
+    film.rental_duration,
+    film.rental_rate,
+    film.length,
+    film.replacement_cost,
+    film.rating,
+    film.last_update,
+    film.special_features,
+    film.fulltext
+ORDER BY
+    film.title ASC;
